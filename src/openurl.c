15f8b7458617c68514873859c3ae93a378a56f67
David Olofson <david@olofson.net>
2016-03-29 00:35:57 +0200

    Added kobo_OpenURL()
    Added a function for opening an URL in the default browser.
    
    Supports Windows ShellExecute() "open", and Un*x system("xdg-open ...").

---
+++ src/openurl.c
@@ -0,0 +1,46 @@
+/*(GPLv2)
+------------------------------------------------------------
+   Kobo Deluxe - An enhanced SDL port of XKobo
+------------------------------------------------------------
+ * Copyright 2016 David Olofson
+ *
+ * This program  is free software; you can redistribute it and/or modify it
+ * under the terms  of  the GNU General Public License  as published by the
+ * Free Software Foundation;  either version 2 of the License,  or (at your
+ * option) any later version.
+ *
+ * This program is  distributed  in  the hope that  it will be useful,  but
+ * WITHOUT   ANY   WARRANTY;   without   even   the   implied  warranty  of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received  a copy of the GNU General Public License along
+ * with this program; if not,  write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "openurl.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <shellapi.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+int kobo_OpenURL(const char *url)
+{
+#ifdef _WIN32
+	HINSTANCE res;
+	CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+			COINIT_DISABLE_OLE1DDE);
+	res = ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL);
+	CoUninitialize();
+	return (res > 32);
+#else
+	char buf[1024];
+	snprintf(buf, sizeof(buf), "xdg-open %s", url);
+	return (system(buf) != -1);
+#endif
+}
---
+++ src/openurl.h
@@ -0,0 +1,35 @@
+/*(GPLv2)
+------------------------------------------------------------
+   Kobo Deluxe - An enhanced SDL port of XKobo
+------------------------------------------------------------
+ * Copyright 2016 David Olofson
+ *
+ * This program  is free software; you can redistribute it and/or modify it
+ * under the terms  of  the GNU General Public License  as published by the
+ * Free Software Foundation;  either version 2 of the License,  or (at your
+ * option) any later version.
+ *
+ * This program is  distributed  in  the hope that  it will be useful,  but
+ * WITHOUT   ANY   WARRANTY;   without   even   the   implied  warranty  of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received  a copy of the GNU General Public License along
+ * with this program; if not,  write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _KOBO_OPENURL_H_
+#define _KOBO_OPENURL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int kobo_OpenURL(const char *url);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* _KOBO_OPENURL_H_ */
