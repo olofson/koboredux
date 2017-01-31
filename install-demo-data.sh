482538d605f90a71c9c3ff07603e697e0814b844
David Olofson <david@olofson.net>
2017-01-31 22:46:15 +0100

    Using 0.7.2 demo data, with EULA
    Bumped demo data version to 0.7.2. (EULA added.)
    
    Added EULA to packages.

--- install-demo-data.sh
+++ install-demo-data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/olofson/koboredux/releases/download/demo-0.7.1/KoboRedux-Demo-0.7.1-Data.tar.bz2
-tar -xjvf KoboRedux-Demo-0.7.1-Data.tar.bz2
-mv KoboRedux-Demo-0.7.1-Data demo-data
+wget https://github.com/olofson/koboredux/releases/download/demo-0.7.2/KoboRedux-Demo-0.7.2-Data.tar.bz2
+tar -xjvf KoboRedux-Demo-0.7.2-Data.tar.bz2
+mv KoboRedux-Demo-0.7.2-Data demo-data
