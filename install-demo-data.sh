af17b36b89971db2ea9346f6e063d44526f286ee
David Olofson <david@olofson.net>
2017-02-01 02:24:54 +0100

    Demo data files are now on koboredux.com
    To avoid the "release doesn't exist when building the release" Catch-22,
    the proprietary demo files are now located in the download folder of the
    koboredux.com site.

--- install-demo-data.sh
+++ install-demo-data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/olofson/koboredux/releases/download/demo-0.7.2/KoboRedux-Demo-0.7.2-Data.tar.bz2
+wget http://koboredux.com/download/KoboRedux-Demo-0.7.2-Data.tar.bz2
 tar -xjvf KoboRedux-Demo-0.7.2-Data.tar.bz2
 mv KoboRedux-Demo-0.7.2-Data demo-data
