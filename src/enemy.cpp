9702c0ca5fc29febc4aed6a69809f53392c02d6e
David Olofson <david@olofson.net>
2017-08-25 00:40:14 +0200

    Adjusting fire/explosion spawn positions for enemy velocity
    KOBO_enemy::explode() now adjusts the fire/explosion spawn positions for
    enemy velocity. Without this, it looks like fast moving enemies jump back
    just before they explode.
    
    Not sure if this solution is entirely correct (one may have to take the
    sprite "tweening" in account as well), but it looks a lot better than
    before.
    
    Closes #467.

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -186,7 +186,7 @@ static const KOBO_ParticleFXDef default_explo_fxdef;
 
 void KOBO_enemy::explode()
 {
-	wfire->NewPSystem(x, y, h, v, &default_explo_fxdef);
+	wfire->NewPSystem(x + h, y + v, h, v, &default_explo_fxdef);
 }
 
 void KOBO_enemy::kill_default()
